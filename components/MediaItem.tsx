46ad2a4b3e8501c4148ff563a67c005cb67bb7b1
Chloe <chloeebubedike@yahoo.com>
2024-05-13 21:22:37 +0100

    Added media item song title and author

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -49,6 +49,16 @@ const MediaItem: React.FC<MediaItemProps> = ({ onClick, data }) => {
           className="object-cover"
         />
       </div>
+      <div
+        className="
+      flex
+      flex-col
+      gap-y-1
+      overflow-hidden"
+      >
+        <p className="text-white truncate">{data.title}</p>
+        <p className="text-neutral-400 text-sm truncate">{data.author}</p>
+      </div>
     </div>
   );
 };
